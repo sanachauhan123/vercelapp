feb242bd3849cedfdff912b3bbb20a0bfa7ab1eb
sanachauhan123 <sanac922@gmail.com>
2025-02-21 12:59:03 +0530

    Fixed CORS issue

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const corsOptions = {
+    origin: "https://reactapp-green-theta.vercel.app", // Your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+  };
+  app.use(cors(corsOptions));
+  
+  app.use(express.json());
 
 app.get("/api", (req, res) => {
     res.send({ message: "Server deployed and running on Vercel." });
